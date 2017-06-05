a02b734b821ceb25cea8a91468c512fc5d4841ce
Cristobal Forno <cforno1@binghamton.edu>
2017-06-04 19:50:29 -0600

    main code is all finished up

--- dump.py
+++ dump.py
@@ -13,37 +13,32 @@ api = tweepy.API(auth)
 
 import enchant
 import re
-import string
 
 dict = enchant.Dict("en_US")
-incorrect = False
 
-def checking(word):
-    if not word:
-        pass
-    elif not dict.check(word):
-        print("WRONG: %s" % word)
-        incorrect = True
-
-for tweet in tweepy.Cursor(api.user_timeline, id="realDonaldTrump").items(50):
+for tweet in tweepy.Cursor(api.user_timeline, id="realDonaldTrump").items(100):
+    incorrect = False
     try:
         new_tweet = str(tweet.text).split()
     except:
         continue
     for word in new_tweet:
         if not word:
+            #checks if the word is empty
             break
         elif word == "RT":
             #ignore word
+            # short for retweet
             break
         elif word == "&amp;":
             #ignore word
+            #for some reason, tweepy interprets the '&' to '&amp;'
             break
         elif word.startswith("#"):
             break
-            #ignore word
+            #ignore word because this is a "trend"
         elif word.startswith("@"):
-            #ignore word
+            #ignore word because this is a username
             break
         elif word.startswith("http"):
             # this is a link...ignore as well
@@ -54,25 +49,43 @@ for tweet in tweepy.Cursor(api.user_timeline, id="realDonaldTrump").items(50):
         elif word == "Facebook":
             # enchant sees this as not a word
             break
+        elif word == "Healthcare":
+            # enchant sees this as not a word
+            break
         elif ("'" in word) and not str(word).startswith("'") and not str(word).endswith("'") :
             # if there is an apostrophe in between a word, it usually means it was a conjuntion
             # if there is an apostrophe at the end or the begininng, it usually means that it is part of a quote
             break
         elif "..." in word:
-            string.replace(word, "...", " ")
-            new_word =str(word).split()
+            if "...." in word:
+                word = str.replace(word, "....", " ")
+            else:
+                word = str.replace(word, "...", " ")
+            new_word = str(word).split()
             for temp in new_word:
-                checking(temp)
+                temp = temp.strip()
+                if not dict.check(temp):
+                    print ("WRONG: %s" % temp)
+                    incorrect = True
             break
         elif "/" in word:
-            string.replace(word, "/", " ")
+            word = str.replace(word, "/", " ")
             new_word =str(word).split()
             for temp in new_word:
-                checking(temp)
+                temp = temp.strip()
+                if not dict.check(temp):
+                    print ("WRONG: %s" % temp)
+                    incorrect = True
             break
         else:
+            #this line removes all special charaters
             word = re.sub('\W+','', word)
-            checking(word)
+            if not word:
+                # if the "word" was only a special character, then it breaks out of the second loop
+                break
+            if not dict.check(word):
+                print ("WRONG: %s" % word)
+                incorrect = True
 
     if incorrect == True:
         print "INCORRECT TWEET: %s" % tweet.text
